26b191485803c528cf10711cf369dba7a2f8ebd0
Irek Solek <intrek@gmail.com>
2019-09-08 16:31:36 +0200

    initial comit

---
+++ cv/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cv
+//
+//  Created by Ireneusz Solek on 08/09/2019.
+//  Copyright © 2019 Ireneusz Solek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
