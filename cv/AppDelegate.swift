a11b7dc51f4b25daecd4015a842d44ad28ef0cc8
Irek Solek <intrek@gmail.com>
2019-09-08 21:36:49 +0200

    Embed OverviewViewController in Navigation VC

--- cv/AppDelegate.swift
+++ cv/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     let initialViewController = OverviewViewController(factory: MockDependencyContainer())
     window = UIWindow(frame: UIScreen.main.bounds)
-    window?.rootViewController = initialViewController
+    window?.rootViewController = UINavigationController(rootViewController: initialViewController)
     window?.makeKeyAndVisible()
     
     return true
