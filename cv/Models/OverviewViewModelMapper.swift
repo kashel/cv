eac55652b90062c4952408e32088c4a563069a53
Irek Solek <intrek@gmail.com>
2019-09-08 17:14:33 +0200

    DefaultOverviewViewModelMapper

--- cv/Models/OverviewViewModelMapper.swift
+++ cv/Models/OverviewViewModelMapper.swift
@@ -15,3 +15,42 @@ protocol OverviewViewModelMapperFactory {
 protocol OverviewViewModelMapper {
   func map(model: CurriculumVitae) -> OverviewViewController.ViewModel
 }
+
+class DefaultOverviewViewModelMapper: OverviewViewModelMapper {
+  typealias Section = OverviewViewController.ViewModel.Section
+  typealias Subsection = OverviewViewController.ViewModel.Subsection
+  
+  private func mapPersonalInformation(_ personalInformation: PersonalInformation) -> Section {
+    let name = "\(personalInformation.firstName) \(personalInformation.lastName)"
+    let firstNameSurname = (title: "First Name / Surname", value: name)
+    return Section(title: "Personal Information",
+                   details: [Subsection(hasDetails: false,
+                                        rows: [firstNameSurname,
+                                               (title: "Phone", value: personalInformation.phone),
+                                               (title: "Nationality", value: personalInformation.nationality),
+                                               (title: "Data of Birth", value: personalInformation.dateOfBirth)])])
+  }
+  
+  private func mapWorkExperience(_ workExperience: WorkExperience) -> Subsection {
+    let responsibilities = workExperience.responsibilities.map{ "- \($0)" }.joined(separator: "\n")
+    return Subsection(hasDetails: true,
+                      rows: [(title: "Dates", value: workExperience.dates),
+                             (title: "Position", value: workExperience.position),
+                             (title: "Responsibilities", value: responsibilities),
+                             (title: "Employer", value: workExperience.employer),
+                             (title: "Address", value: workExperience.address),
+                             (title: "Type of Business", value: workExperience.typeOfBusiness)])
+    
+  }
+  
+  private func mapWorkExperiences(_ workExperiences: [WorkExperience]) -> Section {
+    unowned let unownedSelf = self
+    return Section(title: "Work Experience",
+                   details: workExperiences.map(unownedSelf.mapWorkExperience))
+  }
+  
+  func map(model: CurriculumVitae) -> OverviewViewController.ViewModel {
+    return OverviewViewController.ViewModel(sections: [mapPersonalInformation(model.personalInformation),
+                                                       mapWorkExperiences(model.workExperiences)])
+  }
+}
