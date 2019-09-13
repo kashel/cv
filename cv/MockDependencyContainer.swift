6bb04a61bb2ebc832aa9ce42ddf11fce60d69534
Irek Solek <intrek@gmail.com>
2019-09-13 21:25:33 +0200

    networkDataProvider Tests

--- cv/MockDependencyContainer.swift
+++ cv/MockDependencyContainer.swift
@@ -8,41 +8,6 @@
 
 import Foundation
 
-class MockDependencyContainer {
-  private let mockCV: CurriculumVitae = {
-    let personalInformation = PersonalInformation(firstName: "John",
-                                                  lastName: "Doe",
-                                                  phone: "123456789",
-                                                  email: "test@test.com",
-                                                  nationality: "english",
-                                                  dateOfBirth: "1980")
-    
-    let work1 = WorkExperience(dates: "May 2018 -  September 2019",
-                               position: "iOS Developer",
-                               responsibilities: [
-                                "doing a",
-                                "doing b",
-                                "doing c",
-                                "doing d"],
-                               employer: "Company",
-                               address: "Big city",
-                               typeOfBusiness: "Startup")
-    
-    
-    let education = Education(dates: "2006-2011",
-                              qualification: "Master of Science",
-                              studies: "Computer Science",
-                              institution: "University")
-    
-    
-    let skills = Skills(motherTongue: "English", otherLanguages: ["English"], skills: ["Swif", "Objective C", "GIT"])
-    return CurriculumVitae(personalInformation: personalInformation,
-                           workExperiences: [work1],
-                           edutations: [education],
-                           skills: skills)
-  }()
-}
-
 class DependencyContainer {}
 
 extension DependencyContainer: DataProviderFactory {
